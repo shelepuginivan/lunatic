0d160bd2aa5b77c5901127be042f2921bfcbbd53
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-16 21:49:39 +0300

    feat: add method matchRequest
    Add method that match `req.url` and `route`, `req.method` and expected method and parse dynamic parameters from `req.url`

--- src/router.ts
+++ src/router.ts
@@ -1,1 +1,44 @@
-export class Router {}
+import { Request } from './request';
+import { HttpMethod } from './types/http-method';
+import { Middleware } from './types/middleware';
+
+export class Router {
+	private readonly middlewares: Middleware[];
+
+	constructor() {
+		this.middlewares = [];
+	}
+
+	private matchRequest = (req: Request, method: HttpMethod, route: string) => {
+		if (req.method !== method) {
+			return false;
+		}
+
+		const params: Record<string, string | string[]> = {};
+
+		const routeTokens = route.split('/');
+		let urlTokens = req.url.split('/');
+
+		while (routeTokens.length && urlTokens.length) {
+			const routeToken = routeTokens.shift() as string;
+			const urlToken = urlTokens.shift() as string;
+
+			if (routeToken[0] === ':') {
+				params[routeToken.substring(1)] = urlToken;
+			} else if (routeToken.startsWith('...')) {
+				const paramLength = urlTokens.length - routeTokens.length;
+				params[routeToken.substring(3)] = [urlToken, ...urlTokens.slice(0, paramLength)];
+				urlTokens = urlTokens.slice(paramLength);
+			} else if (routeToken !== urlToken) {
+				return false;
+			}
+		}
+
+		if (routeTokens.length || urlTokens.length) {
+			return false;
+		}
+
+		req.params = params;
+		return true;
+	};
+}
