32c3cd0913ac043aac36e56c19fd982aabf51871
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-16 22:32:09 +0300

    feat: add methods `status` and `end`

--- src/response.ts
+++ src/response.ts
@@ -2,4 +2,14 @@ import * as http from 'http';
 
 export class Response {
 	constructor(private readonly res: http.ServerResponse) {}
+
+	status(status: number): Response {
+		this.res.statusCode = status;
+
+		return this;
+	}
+
+	end() {
+		this.res.end();
+	}
 }
