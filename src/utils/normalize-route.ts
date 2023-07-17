08fbeae9d032edf9694157ecd0b0f02452722c60
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-17 23:26:30 +0300

    fix(utils): change order of route changes
    Add '/' at the start of the route after removing trailing '/' from to prevent it from being an empty string

--- src/utils/normalize-route.ts
+++ src/utils/normalize-route.ts
@@ -1,12 +1,12 @@
 export const normalizeRoute = (route: string) => {
 	route = route.trim();
 
-	if (route[0] !== '/') {
-		route = `/${route}`;
-	}
-
 	if (route.at(-1) === '/') {
 		route = route.slice(0, -1);
+	}
+
+	if (route[0] !== '/') {
+		route = `/${route}`;
 	}
 
 	return route;
