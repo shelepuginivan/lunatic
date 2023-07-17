6f730c0c854b0248ae7f3d3c99b546151f0316be
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-17 14:37:20 +0300

    feat: normalize route when add new middleware

--- src/router.ts
+++ src/router.ts
@@ -88,60 +88,70 @@ export class Router {
 	}
 
 	public use(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'any', route, handler });
 
 		return this;
 	}
 
 	public get(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'GET', route, handler });
 
 		return this;
 	}
 
 	public head(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'HEAD', route, handler });
 
 		return this;
 	}
 
 	public post(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'POST', route, handler });
 
 		return this;
 	}
 
 	public put(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'PUT', route, handler });
 
 		return this;
 	}
 
 	public delete(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'DELETE', route, handler });
 
 		return this;
 	}
 
 	public connect(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'CONNECT', route, handler });
 
 		return this;
 	}
 
 	public options(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'OPTIONS', route, handler });
 
 		return this;
 	}
 
 	public trace(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'TRACE', route, handler });
 
 		return this;
 	}
 
 	public patch(route: string, handler: RequestHandler | Router) {
+		route = normalizeRoute(route);
 		this.middlewares.push({ method: 'PATCH', route, handler });
 
 		return this;
