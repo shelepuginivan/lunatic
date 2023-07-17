594a3225d26c50d7dc918cd100b9172ca3f209e4
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-18 01:24:34 +0300

    lint: specify access modifiers and reorder methods

--- src/response.ts
+++ src/response.ts
@@ -3,33 +3,32 @@ import * as http from 'http';
 export class Response {
 	constructor(private readonly res: http.ServerResponse) {}
 
-	status(status: number): Response {
-		this.res.statusCode = status;
-
-		return this;
-	}
-
-	end() {
+	public end() {
 		this.res.end();
 	}
 
-	json(body: object) {
+	public json(body: object) {
 		this.res.setHeader('Content-Type', 'application/json');
 		this.res.end(JSON.stringify(body));
 	}
 
-	text(body: string) {
-		this.setHeader('Content-Type', 'text/plain');
-		this.res.end(body);
-	}
-
-	setHeader(name: string, value: number | string | string[]) {
+	public setHeader(name: string, value: number | string | string[]) {
 		this.res.setHeader(name, value);
 	}
 
-	setHeaders(headers: Record<string, number | string | string[]>) {
+	public setHeaders(headers: Record<string, number | string | string[]>) {
 		for (const name in headers) {
 			this.res.setHeader(name, headers[name]);
 		}
+	}
+
+	public status(status: number): Response {
+		this.res.statusCode = status;
+		return this;
+	}
+
+	public text(body: string) {
+		this.setHeader('Content-Type', 'text/plain');
+		this.res.end(body);
 	}
 }
