7e1b1b5c74e5f28fe13743d98de4ead971d1a4b7
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-17 16:38:57 +0300

    feat: add methods setHeader and setHeaders

--- src/response.ts
+++ src/response.ts
@@ -17,4 +17,14 @@ export class Response {
 		this.res.setHeader('Content-Type', 'application/json');
 		this.res.end(JSON.stringify(body));
 	}
+
+	setHeader(name: string, value: number | string | string[]) {
+		this.res.setHeader(name, value);
+	}
+
+	setHeaders(headers: Record<string, number | string | string[]>) {
+		for (const name in headers) {
+			this.res.setHeader(name, headers[name]);
+		}
+	}
 }
