5bfc62bc0bfa0730ca0f3f097ad3792e8e555ccd
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-18 00:02:09 +0300

    refactor: move middleware push logic into separate method
    Push middleware in private method addMiddleware to remove duplicated code

--- src/router.ts
+++ src/router.ts
@@ -13,74 +13,44 @@ export class Router {
 		this.middlewares = [];
 	}
 
-	public use(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'any', route, handler });
-
-		return this;
+	public use(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('any', route, handler);
 	}
 
-	public get(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'GET', route, handler });
-
-		return this;
+	public get(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('GET', route, handler);
 	}
 
-	public head(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'HEAD', route, handler });
-
-		return this;
+	public head(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('HEAD', route, handler);
 	}
 
-	public post(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'POST', route, handler });
-
-		return this;
+	public post(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('POST', route, handler);
 	}
 
-	public put(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'PUT', route, handler });
-
-		return this;
+	public put(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('PUT', route, handler);
 	}
 
-	public delete(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'DELETE', route, handler });
-
-		return this;
+	public delete(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('DELETE', route, handler);
 	}
 
-	public connect(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'CONNECT', route, handler });
-
-		return this;
+	public connect(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('CONNECT', route, handler);
 	}
 
-	public options(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'OPTIONS', route, handler });
-
-		return this;
+	public options(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('OPTIONS', route, handler);
 	}
 
-	public trace(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'TRACE', route, handler });
-
-		return this;
+	public trace(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('TRACE', route, handler);
 	}
 
-	public patch(route: string, handler: RequestHandler | Router) {
-		route = normalizeRoute(route);
-		this.middlewares.push({ method: 'PATCH', route, handler });
-
-		return this;
+	public patch(route: string, handler: RequestHandler | Router): this {
+		return this.addMiddleware('PATCH', route, handler);
 	}
 
 
@@ -113,6 +83,12 @@ export class Router {
 		};
 
 		nextHandler();
+	}
+
+	private addMiddleware(method: HttpMethod, route: string, handler: RequestHandler | Router): this {
+		route = normalizeRoute(route);
+		this.middlewares.push({ method, route, handler });
+		return this;
 	}
 
 	private matchRequest(
