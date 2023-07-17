f589d64d7edc924b7425229c4cc8249c572b5d61
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-17 14:30:16 +0300

    feat: add next function in RequestHandler signature

--- src/types/request-handler.ts
+++ src/types/request-handler.ts
@@ -1,4 +1,5 @@
 import { Request } from '../request';
 import { Response } from '../response';
+import { NextHandler } from './next-handler';
 
-export type RequestHandler = (req: Request, res: Response) => unknown
+export type RequestHandler = (req: Request, res: Response, next: NextHandler) => unknown
