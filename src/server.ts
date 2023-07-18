0d0b1b7b04a424bc640ef1b5a0b618d9ece4cb8c
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-18 16:23:30 +0300

    feat: add field renderFunction, its setter, pass renderFunction to the response constructor

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,22 @@ import { createServer, IncomingMessage, Server, ServerResponse } from 'http';
 import { Request } from './request';
 import { Response } from './response';
 import { Router } from './router';
+import { RenderFunction } from './types/render-function';
 
 export class LunaticServer extends Router {
+	public renderFunction: RenderFunction;
 	private readonly httpServer: Server;
 
 	constructor(httpServer?: Server) {
 		super();
 		this.httpServer = httpServer ?? createServer();
+		this.renderFunction = (source: string) => source;
 		this.callback = this.callback.bind(this);
 	}
 
 	public callback(req: IncomingMessage, res: ServerResponse): void {
 		const request = new Request(req);
-		const response = new Response(res);
+		const response = new Response(res, this.renderFunction);
 
 		super.handle(request, response, () => {
 			if (!res.writableEnded) {
@@ -28,5 +31,10 @@ export class LunaticServer extends Router {
 	public listen(port: number): Server {
 		this.httpServer.on('request', this.callback);
 		return this.httpServer.listen(port);
+	}
+
+	public renderer(renderFunction: RenderFunction): this {
+		this.renderFunction = renderFunction;
+		return this;
 	}
 }
