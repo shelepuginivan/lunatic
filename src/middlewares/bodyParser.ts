2d31868fc58db82955ae5f14cc7a782f6cf88b9e
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-18 07:54:53 +0300

    feat: validate Content-Type header
    Check if Content-Type is not application/json or text/plain

--- src/middlewares/bodyParser.ts
+++ src/middlewares/bodyParser.ts
@@ -1,21 +1,24 @@
-import { Request } from '../request';
-import { Response } from '../response';
-import { NextHandler } from '../types/next-handler';
 import { RequestHandler } from '../types/request-handler';
 
-export const bodyParser: RequestHandler = (req: Request, _res: Response, next: NextHandler) => {
+export const bodyParser: RequestHandler = (req, _res, next) => {
+	const contentType = req.headers['content-type'];
+
+	if (contentType !== 'application/json' || contentType !== 'text/plain') {
+		return next();
+	}
+
 	const chunks: Uint8Array[] = [];
 
-	req.on('data', (chunk) => {
-		chunks.push(chunk);
-	}).on('end', () => {
-		let body: string | object = Buffer.concat(chunks).toString();
+	req
+		.on('data', (chunk) => chunks.push(chunk))
+		.on('end', () => {
+			let body: string | object = Buffer.concat(chunks).toString();
 
-		if (req.headers['content-type'] === 'application/json') {
-			body = JSON.parse(body);
-		}
+			if (contentType === 'application/json') {
+				body = JSON.parse(body);
+			}
 
-		req.body = body;
-		next();
-	});
+			req.body = body;
+			next();
+		});
 };
