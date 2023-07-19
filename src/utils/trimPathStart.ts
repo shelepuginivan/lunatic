3af72b6eeaf7bbe6ae07a98121cb6e6fbfbfba1c
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-19 19:53:03 +0300

    feat(utils): add utility function trimPathStart

---
+++ src/utils/trimPathStart.ts
@@ -0,0 +1,15 @@
+import { normalizeRoute } from './normalize-route';
+
+export const trimPathStart = (path: string, pathStart: string) => {
+	path = normalizeRoute(path);
+	pathStart = normalizeRoute(pathStart);
+
+	if (pathStart === '/') {
+		return path;
+	}
+
+	const pathStartLength = pathStart.split('/').length
+	const pathTokens = path.split('/')
+
+	return normalizeRoute(pathTokens.slice(pathStartLength).join('/'));
+}
