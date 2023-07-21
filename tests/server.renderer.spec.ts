204cfb6633430cdb834ac88a5d770a42d1be4f9f
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-21 16:05:26 +0300

    tests: add tests for LunaticServer.renderer() method

---
+++ tests/server.renderer.spec.ts
@@ -0,0 +1,84 @@
+import { beforeEach, describe, it } from '@jest/globals';
+import { Server } from 'http';
+import request from 'supertest';
+
+import { LunaticServer, RenderFunction } from '../src';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+describe('LunaticServer.renderer()', () => {
+	let app: LunaticServer;
+	let server: Server;
+
+	beforeEach(() => {
+		app = new LunaticServer();
+		server = new Server(app.callback);
+	});
+
+	it('Should render the exact content by default', async () => {
+		const html = '<h1>Hello world</h1>'
+
+		app.get('/', (_req, res) => {
+			res.render(html);
+		});
+
+		await request(server)
+			.get('/')
+			.expect('Content-Type', 'text/html')
+			.expect(html);
+	});
+
+	it('Should be able to use custom render functions', async () => {
+		const html = '<h1>{{ value }}</h1>'
+
+		const renderFunction: RenderFunction = (source, options) => {
+			for (const key in options) {
+				source = source.replace(`{{ ${key} }}`, options[key]);
+			}
+
+			return source;
+		}
+
+		app.renderer(renderFunction);
+
+		app.get('/', (_req, res) => {
+			res.render(html, {
+				value: 'Hello custom render function'
+			});
+		});
+
+		await request(server)
+			.get('/')
+			.expect('Content-Type', 'text/html')
+			.expect('<h1>Hello custom render function</h1>');
+	});
+
+	it('Should be able to render files', async () => {
+		const pathToHtml = join(__dirname, 'mocks', 'files', '2.html');
+		const htmlContent = await readFile(pathToHtml);
+		const html = htmlContent.toString();
+
+		const expectedHtml = html.replace('{{ value }}', 'foo');
+
+		const renderFunction: RenderFunction = (source, options) => {
+			for (const key in options) {
+				source = source.replace(`{{ ${key} }}`, options[key]);
+			}
+
+			return source;
+		}
+
+		app.renderer(renderFunction);
+
+		app.get('/', (_req, res) => {
+			res.renderFile(pathToHtml, {
+				value: 'foo'
+			});
+		});
+
+		await request(server)
+			.get('/')
+			.expect('Content-Type', 'text/html')
+			.expect(expectedHtml);
+	})
+});
