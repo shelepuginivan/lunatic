b6992309294d1ba1a506b8681741f86f0e962079
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-23 11:51:59 +0300

    feat: add configuration support

--- src/middlewares/serve-static.ts
+++ src/middlewares/serve-static.ts
@@ -1,11 +1,94 @@
-import { join } from 'path';
+import { createHash } from 'crypto';
+import { existsSync } from 'fs';
+import { stat } from 'fs/promises';
+import { join, sep } from 'path';
 
 import { Router } from '../router';
 
-export const serveStatic = (staticDir: string): Router => {
+export interface ServeStaticOptions {
+	dotfiles?: 'allow' | 'forbid' | 'ignore'
+	etag?: boolean
+	index?: string | false
+	lastModified?: boolean
+}
+
+export const serveStatic = (staticDir: string, options?: ServeStaticOptions): Router => {
+	options = options ?? {
+		dotfiles: 'ignore',
+		etag: true,
+		index: 'index.html',
+		lastModified: true
+	};
+
+	const { dotfiles, etag, index, lastModified } = options;
+
 	return new Router()
 		.get('*', async (req, res) => {
-			const fullPath = join(staticDir, req.path);
+			let fullPath = join(staticDir, req.path);
+
+			if (
+				dotfiles !== 'allow' &&
+				fullPath.split(sep).some((part) => part.startsWith('.'))
+			) {
+				const status = dotfiles === 'ignore'
+					? 404
+					: 403;
+
+				return await res.status(status).end();
+			}
+
+			const stats = existsSync(fullPath) && await stat(fullPath);
+
+			if (!stats) {
+				return await res.status(404).end();
+			}
+
+			if (stats.isDirectory()) {
+				if (index) {
+					const pathToIndex = join(fullPath, index);
+
+					if (existsSync(pathToIndex)) {
+						fullPath = pathToIndex;
+					}
+				} else {
+					return await res.status(404).end();
+				}
+			}
+
+			if (lastModified) {
+				res.setHeader('Last-Modified', stats.mtime.toUTCString());
+			}
+
+			if (etag) {
+				const hash = createHash('md5');
+				hash.update(stats.mtime.toUTCString());
+				const etagValue = `"${hash.digest('hex')}"`;
+
+				const ifNoneMatchHeader = req.headers['if-none-match'];
+
+				if (
+					ifNoneMatchHeader &&
+					ifNoneMatchHeader.split(',').some((tag) =>
+						tag.trim() === etagValue
+					)
+				) {
+					return await res.status(304).end();
+				}
+
+				const ifMatchHeader = req.headers['if-match'];
+
+				if (
+					ifMatchHeader &&
+					ifMatchHeader.split(',').every((tag) =>
+						tag.trim() !== etagValue
+					)
+				) {
+					return await res.status(412).end();
+				}
+
+				res.setHeader('ETag', etagValue);
+			}
+
 			await res.status(200).sendFile(fullPath);
 		})
 		.use('*', async (_req, res) => {
